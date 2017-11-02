8081816dde165d315ea46f3061219ec0ef344b9b
SeregaZnam <Sergey-znamensk@yandex.ru>
2017-11-02 23:43:22 +0400

    step 1

---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+var gulp         = require('gulp'),
+	less         = require('gulp-less'),
+	autoprefixer = require('gulp-autoprefixer'),
+	cleanCSS     = require('gulp-clean-css'),
+	rename       = require('gulp-rename'),
+	browserSync  = require('browser-sync').create(),
+	concat       = require('gulp-concat'),
+	uglify       = require('gulp-uglify');
+
+gulp.task('browser-sync', ['styles', 'scripts'], function() {
+		browserSync.init({
+				proxy: "optimized.dev",
+				notify: false
+		});
+});
+
+gulp.task('styles', function () {
+	return gulp.src('less/**/*.less')
+	.pipe(less({
+		includePaths: require('node-bourbon').includePaths
+	}))
+	.pipe(rename({suffix: '.min', prefix : ''}))
+	.pipe(autoprefixer({browsers: ['last 15 versions'], cascade: false}))
+	.pipe(cleanCSS())
+	.pipe(gulp.dest('app/css'))
+	.pipe(browserSync.stream());
+});
+
+gulp.task('scripts', function() {
+	return gulp.src('./app/libs/**/*.js')
+		.pipe(concat('main.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest('./app/js/'))
+});
+
+gulp.task('watch', function () {
+	gulp.watch('less/*.less', ['styles']);
+	gulp.watch('app/libs/**/*.js', ['scripts']);
+	gulp.watch('app/js/*.js').on("change", browserSync.reload);
+	gulp.watch('app/**/*.php').on('change', browserSync.reload);
+});
+
+gulp.task('default', ['browser-sync', 'watch']);
